a6ccdbf0c078fde6b5603cc1895e7f2eea31019e
cwastche <cwastche@student.s19.be>
2023-09-07 16:30:59 +0200

    Nearly done, just needs to include npx migrate into the DOckerfiles.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,16 +3,18 @@
 
 // module.exports = nextConfig
 
+backend = "backend-" + process.env.MODE
 module.exports = {
     async rewrites() {
+	console.log(backend)
       return [
         {
           source: '/api/auth/start-oauth',
-          destination: 'http://backend-dev:3000/auth/start-oauth',
+          destination: `http://${backend}:3000/auth/start-oauth`,
         },
 		{
 			source: '/token/auth/oauth-token',
-			destination: 'http://localhost:3000/auth/oauth-token',
+			destination: `http://${backend}:3000/auth/oauth-token`,
 		},
       ];
     },
