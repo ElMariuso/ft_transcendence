d181948a7560e46284ed76ea15d5130cb6c1f41e
ElMariuso <thiry.marius@gmail.com>
2023-09-12 20:05:29 +0200

    app module update

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { WebSocketModule } from '@nestjs/websockets';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from 'src/Module/user.module';
+import { GameModule } from './Module/game.module';
 import { AuthController } from './Controller/auth.controller';
+// import { GameController } from './game/game.controller';
+// import { GameService } from './game/game.service';
 
 @Module({
-  imports: [],
+  imports: [
+    WebSocketModule.forRoot({
+      namespace: 'matchmaking',
+      serveClient: true,
+    }),
+  ],
   controllers: [AppController, AuthController],
   providers: [AppService],
 })
