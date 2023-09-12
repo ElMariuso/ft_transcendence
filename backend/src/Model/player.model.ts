5d2dbef5188367298b79bc4d4e11db4a75283f6f
ElMariuso <thiry.marius@gmail.com>
2023-09-12 17:41:04 +0200

    Models

---
+++ backend/src/Model/player.model.ts
@@ -0,0 +1,15 @@
+/**
+ * Player Model
+ * Represents a player in the system with a unique identifier and associated points.
+ */
+export class PlayerInQueue {
+    /**
+     * The unique identifier for the player.
+     */
+    id: number;
+
+    /**
+     * The points or score associated with the player.
+     */
+    points: number;
+}
