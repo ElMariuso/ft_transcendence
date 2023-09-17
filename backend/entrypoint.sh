b5076d2bbf20e3647678d91d093945240433b701
ElMariuso <thiry.marius@gmail.com>
2023-09-17 16:29:28 +0200

    Update

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 
+# Import environment variables
 source .env
 
-# Check if an environment argument is provided
+# Ensure an environment argument is provided
 if [ -z "$1" ]; then
     echo "Usage: $0 {dev|prod}"
     exit 1
 fi
 
-# Check if a valid environment argument is provided
+# Ensure the environment argument is valid
 if [ "$1" != "dev" ] && [ "$1" != "prod" ]; then
     echo "Usage: $0 {dev|prod}"
     exit 1
 fi
 
 # Function to wait for PostgreSQL to be ready
-# wait_for_postgres() {
-#     until pg_isready -h $DB_HOST -p $DB_PORT -U $DB_USER; do
-#         echo "Waiting for PostgreSQL to be ready..."
-#         sleep 1
-#     done
-# }
+wait_for_postgres() {
+    while ! pg_isready -h $DB_HOST -p $DB_PORT -U $DB_USER; do
+        echo "$(date +'%Y-%m-%d %H:%M:%S') - Waiting for PostgreSQL to be ready..."
+        sleep 10
+    done
+}
 
-# # Wait for PostgreSQL
-# wait_for_postgres
+# Wait for PostgreSQL
+wait_for_postgres
 
 # Run Prisma migration based on the environment argument
 if [ "$1" == "dev" ]; then
     # Run Prisma migration for development
     npx prisma migrate dev
 elif [ "$1" == "prod" ]; then
-    # Run Prisma migration for production (deploy)
-    # npx prisma migrate deploy
-	npx prisma migrate dev
+    # Run Prisma migration for production
+    npx prisma migrate deploy
 fi
 
 # Start your application based on the environment argument
