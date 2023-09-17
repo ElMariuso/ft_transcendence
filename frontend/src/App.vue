36cc1b19716d3ed0f0e52a4effa62570bdb360e6
ElMariuso <thiry.marius@gmail.com>
2023-09-18 00:46:49 +0200

    Update

---
+++ frontend/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import axios from 'axios';
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      posts: []
+    };
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+      this.posts = response.data;
+    });
+  }
+};
+</script>
+
+<style>
+@import './assets/styles.css';
+</style>
--- frontend/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-// import RootLayout from './layout';
-
-export default function Home() {
-  return (
-    <p>test</p>
-  );
-}
