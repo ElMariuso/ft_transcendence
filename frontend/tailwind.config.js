5df6b5d531de050009ce41c110839a18b9452fea
ElMariuso <thiry.marius@gmail.com>
2023-09-18 19:49:06 +0200

    Main background

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,8 +2,16 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}", ],
   theme: {
-      extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-gradient': "linear-gradient(to bottom, hsl(37, 12%, 16%), hsl(37, 10%, 8%) 116px)",
+      },
+      backgroundColor: {
+        'base-bg': '#161512',
+      }
+    }
   },
+  variants: {},
   plugins: [],
 }
 
