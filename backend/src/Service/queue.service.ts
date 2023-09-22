497e0a6e8a071fc658b9b7316cf8fdc7cb0ce9b9
ElMariuso <thiry.marius@gmail.com>
2023-09-22 20:37:47 +0200

    update queue.service.ts

--- backend/src/Service/queue.service.ts
+++ backend/src/Service/queue.service.ts
@@ -1,56 +1,94 @@
 import { Injectable } from '@nestjs/common';
-import { PlayerInQueue } from 'src/Model/player.model';
+import { AuthenticatedPlayer, PlayerInQueue } from 'src/Model/player.model';
 
 /**
  * The QueueService handles the management of the queue for players waiting for a match.
  */
 @Injectable()
 export class QueueService {
-    // An array that represents the current queue of players.
-    private queue: PlayerInQueue[] = [];
+    private standardQueue: PlayerInQueue[] = [];
+    private rankedQueue: AuthenticatedPlayer[] = [];
 
+    /* Standard Queue */
     /**
-     * Adds a player to the end of the queue.
+     * Adds a player to the standard queue.
      * 
-     * @param player - The player to be added to the queue.
+     * @param player - The player to be added to the standard queue.
      */
     add(player: PlayerInQueue): void {
-        this.queue.push(player);
+        this.standardQueue.push(player);
     }
 
     /**
-     * Removes a player from the queue based on their ID.
+     * Adds a player to the ranked queue.
      * 
-     * @param playerId - The ID of the player to be removed from the queue.
+     * @param player - The authenticated player to be added to the ranked queue.
      */
-    remove(playerId: number): void {
-        this.queue = this.queue.filter(player => player.id !== playerId);
+    remove(playerId: string | number): void {
+        this.standardQueue = this.standardQueue.filter(player => player.id !== playerId);
     }
 
     /**
-     * Retrieves the current size of the queue.
+     * Retrieves the current size of the standard queue.
      * 
-     * @returns The number of players currently in the queue.
+     * @returns The number of players currently in the standard queue.
      */
     getQueueSize(): number {
-        return this.queue.length;
+        return this.standardQueue.length;
     }
 
     /**
-     * Getter for the current queue of players waiting for a match.
+     * Getter for the current players waiting in the standard queue.
      * 
-     * @returns The array of players in the queue.
+     * @returns The array of players in the standard queue.
      */
-    get currentQueue(): PlayerInQueue[] {
-        return this.queue;
+    getCurrentQueue(): PlayerInQueue[] {
+        return this.standardQueue;
+    }
+
+    /* Ranked Queue */
+    /**
+     * Adds a player to the ranked queue.
+     * 
+     * @param player - The authenticated player to be added to the ranked queue.
+     */
+    addRanked(player: AuthenticatedPlayer): void {
+        this.rankedQueue.push(player);
     }
 
     /**
-     * Retrieves the players from the queue and sorts them in ascending order based on their points.
+     * Removes a player from the ranked queue based on their ID.
      * 
-     * @returns A sorted array of players, with players having the least points appearing first.
+     * @param playerId - The ID of the player to be removed from the ranked queue.
      */
-    getSortedQueue(): PlayerInQueue[] {
-        return this.queue.sort((a, b) => a.points - b.points);
+    removeRanked(playerId: number): void {
+        this.rankedQueue = this.rankedQueue.filter(player => player.id !== playerId);
+    }
+
+    /**
+     * Retrieves the current size of the ranked queue.
+     * 
+     * @returns The number of authenticated players currently in the ranked queue.
+     */
+    getRankedQueueSize(): number {
+        return this.rankedQueue.length;
+    }
+
+    /**
+     * Getter for the current players waiting in the ranked queue.
+     * 
+     * @returns The array of players in the ranked queue.
+     */
+    getCurrentRankedQueue(): AuthenticatedPlayer[] {
+        return this.rankedQueue;
+    }
+
+    /**
+     * Getter for the current players waiting in the ranked queue, sorted by points.
+     * 
+     * @returns A sorted array of authenticated players, with players having the least points appearing first.
+     */
+    getSortedRankedQueue(): AuthenticatedPlayer[] {
+        return this.rankedQueue.sort((a, b) => a.points - b.points);
     }
 }
