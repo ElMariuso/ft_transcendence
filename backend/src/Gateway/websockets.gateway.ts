adb746d0b038aa8686e7fa159f12d6e5a99bf877
ElMariuso <thiry.marius@gmail.com>
2023-09-24 18:51:14 +0200

    Update

--- backend/src/Gateway/websockets.gateway.ts
+++ backend/src/Gateway/websockets.gateway.ts
@@ -1,9 +1,29 @@
 import { SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from '@nestjs/websockets';
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["header-test"],
+    credentials: true
+  }
+})
 export class WebSocketsGateway {
+  @WebSocketServer() server;
+
+  constructor() {
+    console.log("WebSocketsGateway initialized");
+  }
+
   @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  handleMessage(client: any, payload: any): void {
+    console.log('Message reçu:', payload);
+    client.emit('message_response', 'Hello world!');
+  }
+
+  @SubscribeMessage('events')
+  handleEvent(client: any, data: any): WsResponse<any> {
+    console.log('Événement reçu:', data);
+    return { event: 'events', data: 'Test' };
   }
 }
