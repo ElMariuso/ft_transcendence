ff3cc0011a9dd119b252d74b552dd0a8a236b727
cwastche <cwastche@student.s19.be>
2023-10-16 15:57:39 +0200

    cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,9 +5,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  const prisma = new PrismaClient();
+  app.enableCors({
+    origin: 'http://localhost:8080'
+  });
 
+  const prisma = new PrismaClient();
   app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
     req['prisma'] = prisma;
     next();
   });
