9cf2012655c41dca3ed5f189c5986a101709ef81
ElMariuso <thiry.marius@gmail.com>
2023-10-17 19:19:33 +0200

    MatchmakingBox integred

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,8 +3,10 @@ import { ref, onMounted } from 'vue';
 import { v4 as uuidv4 } from 'uuid';
 import Cookies from 'js-cookie';
 import Navbar from './components/Navbar.vue';
+import MatchmakingBox from './components/MatchmakingBox.vue';
 
 const guestUUID = ref<string>('');
+const isSearchingValue = ref<boolean>(false);
 
 onMounted(() => {
     let storedUUID = Cookies.get('guestUUID');
@@ -13,6 +15,10 @@ onMounted(() => {
         Cookies.set('guestUUID', storedUUID, { expires: 365 });
     }
     guestUUID.value = storedUUID;
+
+    const storedIsSearching = Cookies.get('isSearching');
+    isSearchingValue.value = storedIsSearching === 'true';
+    console.log("RET:", isSearchingValue.value);
 });
 </script>
 
@@ -23,6 +29,8 @@ onMounted(() => {
     <div class="container mx-auto mt-8">
       <router-view />
       <div class="text-center mt-96">UUID: {{ guestUUID }}</div> 
+      <div class="text-center mt-96">isSearching: {{ isSearchingValue }}</div> 
     </div>
   </div>
+  <MatchmakingBox v-if="isSearchingValue" />
 </template>
