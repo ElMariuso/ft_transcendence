fd95d5e8b33297362a6da1c19ccd077fe0829642
ElMariuso <thiry.marius@gmail.com>
2023-10-18 14:38:53 +0200

    Config websocket client side

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,10 +3,14 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia';
 import App from './App.vue'
 import router from './router'
+import socket from './services/socket-helpers';
 
 /* Create Vue app */
 const app = createApp(App);
 const pinia = createPinia();
+
+/* Config websocket client side */
+app.config.globalProperties.$socket = socket;
 
 /* Use router and store if applicable */
 app.use(router)
@@ -14,5 +18,3 @@ app.use(pinia)
 
 /* Mount the app to the DOM */
 app.mount('#app')
-
-
