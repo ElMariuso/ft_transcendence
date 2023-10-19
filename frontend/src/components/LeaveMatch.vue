2499cb4bfba8814febae59471742fea202c69e86
ElMariuso <thiry.marius@gmail.com>
2023-10-19 19:40:12 +0200

    LeaveMatch component

---
+++ frontend/src/components/LeaveMatch.vue
@@ -0,0 +1,23 @@
+<script setup lang='ts'>
+import { quitRankedMatch, quitStandardMatch } from '@/services/matchmaking-helpers';
+import { useMatchmakingStore } from '@/stores/MatchmakingStore';
+
+const matchmakingStore = useMatchmakingStore();
+
+const quitMatch = async () => {
+    console.log('Leaving the match...');
+    try {
+        if (matchmakingStore.isRanked) {
+            await quitRankedMatch();
+        } else {
+            await quitStandardMatch();
+        }
+    } catch (error) {
+        console.error(error);
+    }
+};
+</script>
+
+<template>
+    <button @click="quitMatch">Leave</button>
+</template>
--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -1,26 +1,5 @@
-<script setup lang='ts'>
-import { quitRankedMatch, quitStandardMatch } from '@/services/matchmaking-helpers';
-import { useMatchmakingStore } from '@/stores/MatchmakingStore';
-
-const matchmakingStore = useMatchmakingStore();
-
-const quitMatch = async () => {
-    console.log('Leaving the match...');
-    try {
-        if (matchmakingStore.isRanked) {
-            await quitRankedMatch();
-        } else {
-            await quitStandardMatch();
-        }
-    } catch (error) {
-        console.error(error);
-    }
-};
-</script>
-
 <template>
     <div>
         <h1>This is the game page</h1>
-        <button @click="quitMatch">Salut</button>
     </div>
 </template>
