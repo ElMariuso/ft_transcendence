be556d75cd4f904fea0c5d28f0586cb4670cea22
ElMariuso <thiry.marius@gmail.com>
2023-11-29 15:24:13 +0100

    Documentation of gameloop service

--- backend/src/Service/gameloop.service.ts
+++ backend/src/Service/gameloop.service.ts
@@ -4,11 +4,19 @@ import { GameStateService } from './gamestate.service';
 @Injectable()
 export class GameLoopService {
     private readonly logger = new Logger(GameLoopService.name);
+
+    // Interval object for the game loop, allowing it to be cleared when needed.
     private gameLoopInterval: NodeJS.Timeout | null = null;
+
+    // The tick rate for the game loop (number of updates per second).
     private readonly TICK_RATE = 1000 / 120;
 
     constructor(private readonly gameStateService: GameStateService) {}
 
+    /**
+     * Starts the game loop if both players are ready.
+     * Sets an interval that updates the game state at the defined tick rate.
+     */
     startGameLoop(): void {
         if (this.gameStateService.playerReady.first && this.gameStateService.playerReady.second) {
             this.gameStateService.displayBall();
@@ -19,15 +27,27 @@ export class GameLoopService {
         }
     }
 
+    /**
+     * Stops the game loop by clearing the interval.
+     * This is typically called when a game ends or is paused.
+     */
     stopGameLoop(): void {
         clearInterval(this.gameLoopInterval);
         this.logger.log('Game loop stopped');
     }
 
+    /**
+     * Returns the current game state.
+     * Useful for retrieving game data outside of the game loop.
+     */
     getGameState() {
         return this.gameStateService;
     }
 
+    /**
+     * A single tick of the game loop.
+     * Updates the ball position and handles any potential errors.
+     */
     private gameTick(): void {
         try {
           this.gameStateService.updateBallPosition();
